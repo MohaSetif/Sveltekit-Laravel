c0dbb937b718a4087deffb98ba57c03b77a3f905
MohaSetif <bouroubamohamed004@gmail.com>
2023-08-31 23:15:22 +0100

    0.0.1

---
+++ sveltekit/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "$lib/css/main.css"
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/users">Our Users</a>
